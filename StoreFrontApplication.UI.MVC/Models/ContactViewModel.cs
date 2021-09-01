113910359aae7be9970455498a1351537cea521f
Jacob Roelofsz <jroelofsz@outlook.com>
2021-09-01 15:06:32 -0500

    Converted Template to MVC

---
+++ StoreFrontApplication.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StoreFrontApplication.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is required")]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "* Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* A message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+
+    }
+}
