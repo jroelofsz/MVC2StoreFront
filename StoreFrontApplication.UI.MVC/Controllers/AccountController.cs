2eea97c0494fb630bc90d1b81ce479ec2e7572a1
Jacob Roelofsz <jroelofsz@outlook.com>
2021-09-01 18:35:03 -0500

    Fixed issue with Registration

--- StoreFrontApplication.UI.MVC/Controllers/AccountController.cs
+++ StoreFrontApplication.UI.MVC/Controllers/AccountController.cs
@@ -151,14 +151,16 @@ namespace StoreFrontApplication.UI.MVC.Controllers
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    var code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
-                    ViewBag.Link = callbackUrl;
-                    return View("DisplayEmail");
-                }
+                if (result.Succeeded){
+                    //{
+                    //    var code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    //    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    //    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
+                    //    ViewBag.Link = callbackUrl;
+                    //    return View("DisplayEmail");
+                    UserManager.AddToRole(user.Id, "Customer");
+                    return RedirectToAction("Login");
+            }
                 AddErrors(result);
             }
 
