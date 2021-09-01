7d6f4f9ef1f69db53460d70af6aabddbc0433630
Jacob Roelofsz <jroelofsz@outlook.com>
2021-09-01 16:08:19 -0500

    Added some CSS bundles

--- StoreFrontApplication.UI.MVC/App_Start/BundleConfig.cs
+++ StoreFrontApplication.UI.MVC/App_Start/BundleConfig.cs
@@ -8,10 +8,13 @@ namespace StoreFrontApplication.UI.MVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Content/js/demo.js",
+                        "~/Content/js/front.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -19,12 +22,21 @@ namespace StoreFrontApplication.UI.MVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Scripts/respond.js",
+                      "~/Content/vendor/bootstrap-select/css/bootstrap-select.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/vendor/lightbox2/css/lightbox.min.css",
+                      "~/Content/vendor/nouislider/nouislider.min.css",
+                      "~/Content/vendor/owl.carousel2/assets/owl.carousel.min.css",
+                      "~/Content/vendor/owl.carousel2/assets/owl.theme.default.css",
+                      "~/Content/css/style.default.css",
+                      "~/Content/img/favicon.png",
+                      "~/Content/css/jakecustom.css"));
         }
+
     }
 }
